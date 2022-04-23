59341282531310e537ae4430d0c413a7ecf9ebcc
Fernanda Vidal <fevidal.dev@gmail.com>
2022-04-23 15:45:14 -0300

    Adiciona requisito 4.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const rgb = `(${(Math.floor(Math.random() * 554))}, ${(Math.floor(Math.random() * 554))}, ${(Math.floor(Math.random() * 554))})`;
 
 document.getElementById('rgb-color').innerText = rgb;
+
+document.getElementById('ball1').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
+${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
+document.getElementById('ball2').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
+${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
+document.getElementById('ball3').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
+${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
+document.getElementById('ball4').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
+${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
+document.getElementById('ball5').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
+${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
+document.getElementById('ball6').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
+${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
