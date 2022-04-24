200476610af3ea881f82d0c1a50710cad76d080b
Fernanda Vidal <fevidal.dev@gmail.com>
2022-04-23 23:23:31 -0300

    Adiciona requisito 6 (requisito 5 incompleto).

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-const rgb = `(${(Math.floor(Math.random() * 554))}, ${(Math.floor(Math.random() * 554))}, ${(Math.floor(Math.random() * 554))})`;
+const colorTarget = document.getElementById('rgb-color');
 
-document.getElementById('rgb-color').innerText = rgb;
+const ball1 = document.getElementById('ball1');
+const ball2 = document.getElementById('ball2');
+const ball3 = document.getElementById('ball3');
+const ball4 = document.getElementById('ball4');
+const ball5 = document.getElementById('ball5');
+const ball6 = document.getElementById('ball6');
 
-document.getElementById('ball1').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
-${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
-document.getElementById('ball2').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
-${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
-document.getElementById('ball3').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
-${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
-document.getElementById('ball4').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
-${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
-document.getElementById('ball5').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
-${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
-document.getElementById('ball6').style.backgroundColor = `rgb(${(Math.floor(Math.random() * 554))},
-${(Math.floor(Math.random() * 554))},${(Math.floor(Math.random() * 554))})`;
+const divBalls = [ball1, ball2, ball3, ball4, ball5, ball6];
+
+// console.log(divBalls);
+
+const colorRandom = () => {
+  const color1 = (Math.floor(Math.random() * 254));
+  const color2 = (Math.floor(Math.random() * 254));
+  const color3 = (Math.floor(Math.random() * 254));
+  return `(${color1}, ${color2}, ${color3})`;
+};
+// console.log(colorRandom())
+
+colorTarget.innerHTML = `${colorRandom()}`;
+
+const pickColor = (ball) => {
+  let newBall = ball;
+  let pick = 'rgb'.concat(colorRandom());
+  newBall.style.backgroundColor = pick;
+  return newBall;
+};
+
+const getBall = () => {
+   const ball = divBalls.forEach((elemento) => pickColor(elemento));
+   return ball;
+};
+getBall();
+
+const verifyColor = (event) => {
+  const evento = event.target.style.backgroundColor;
+  const correto = document.getElementById('rgb-color').innerText;
+  const paragrafo = document.getElementById('answer');
+  if (evento === correto) {
+    paragrafo.innerText = 'Acertou!';
+    return paragrafo;
+  }
+  paragrafo.innerText = 'Errou! Tente novamente!';
+  return paragrafo;
+};
+ball1.addEventListener('click', verifyColor);
+ball2.addEventListener('click', verifyColor);
+ball3.addEventListener('click', verifyColor);
+ball4.addEventListener('click', verifyColor);
+ball5.addEventListener('click', verifyColor);
+ball6.addEventListener('click', verifyColor);
+
+const button = document.getElementById('reset-game');
+
+const startOver = () => {
+  getBall();
+  colorTarget.innerHTML = `${colorRandom()}`;
+  document.getElementById('answer').innerText = 'Escolha uma cor';
+};
+button.addEventListener('click', startOver);
